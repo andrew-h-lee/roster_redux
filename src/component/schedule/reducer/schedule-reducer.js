6803aac097b3e440a55755aaeb9407afd34efc71
andrew-h-lee <mr.ahlee@gmail.com>
2016-11-09 00:52:35 +1100

    Upload Data: Update

--- src/component/schedule/reducer/schedule-reducer.js
+++ src/component/schedule/reducer/schedule-reducer.js
@@ -5,7 +5,7 @@ import { ADD, REMOVE } from '../action/schedule-action';
 const initialState =[{
   title: 'Registration & Breakfast',
   description: 'Kick off the day with a Big Breakfast',
-  speaker: '',
+  speaker: 'Conference Announcement',
   time: {
     start: "2016-09-03T08:30:00.0Z",
     end: "2016-09-03T09:30:00.0Z"
@@ -17,8 +17,8 @@ const initialState =[{
     description: 'Making Easy = Right',
     speaker: 'Alexandra Spillane',
     time: {
-      start: "2016-09-03T09:30:00.0Z",
-      end: "2016-09-03T10:10:00.0Z"
+      start: "2016-09-03T09:40:00.0Z",
+      end: "2016-09-03T10:30:00.0Z"
     },
     track: 'a',
     id: 1
@@ -27,8 +27,8 @@ const initialState =[{
     description: 'The Key to Designing Reliable Systems',
     speaker: 'Indu Alagarsamy',
     time: {
-      start: "2016-09-03T09:30:00.0Z",
-      end: "2016-09-03T10:10:00.0Z"
+      start: "2016-09-03T09:40:00.0Z",
+      end: "2016-09-03T10:30:00.0Z"
     },
     track: 'b',
     id: 2
@@ -37,8 +37,8 @@ const initialState =[{
     description: 'Whether, When & How to Migrate',
     speaker: 'Randy Shoup',
     time: {
-      start: "2016-09-03T09:30:00.0Z",
-      end: "2016-09-03T10:10:00.0Z"
+      start: "2016-09-03T09:40:00.0Z",
+      end: "2016-09-03T10:30:00.0Z"
     },
     track: 'c',
     id: 3
@@ -47,62 +47,92 @@ const initialState =[{
     description: 'Lessons from the Spark Project',
     speaker: 'Dean Wampler',
     time: {
-      start: "2016-06-03T11:10:00.0Z",
-      end: "2016-06-03T11:50:00.0Z"
+      start: "2016-09-03T11:10:00.0Z",
+      end: "2016-09-03T12:00:00.0Z"
     },
     track: 'a',
     id: 4
   }, {
+    title: 'Delivery Mapping',
+    description: 'Turning the Lights On',
+    speaker: 'Dan North',
+    time: {
+      start: "2016-09-03T11:10:00.0Z",
+      end: "2016-09-03T12:00:00.0Z"
+    },
+    track: 'b',
+    id: 5
+  }, {
+    title: 'Sustain Support Data Variability',
+    description: 'Keeping Components Clean while Dealing with Data Variability',
+    speaker: 'Joseph Yolder',
+    time: {
+      start: "2016-09-03T11:10:00.0Z",
+      end: "2016-09-03T12:00:00.0Z"
+    },
+    track: 'c',
+    id: 6
+  }, {
+    title: 'Big Wins with Small Data',
+    description: 'Prediction.io in Ecommerce',
+    speaker: 'David Jones',
+    time: {
+      start: "2016-09-03T12:10:00.0Z",
+      end: "2016-09-03T13:00:00.0Z"
+    },
+    track: 'a',
+    id: 7
+  }, {
+    title: 'Azure Machine Learning',
+    description: 'Anatomy of Machine Learning Service',
+    speaker: 'Sharat Chikkerur',
+    time: {
+      start: "2016-09-03T12:10:00.0Z",
+      end: "2016-09-03T13:00:00.0Z"
+    },
+    track: 'b',
+    id: 8
+  }, {
+    title: 'Protocols & Structures for Inference',
+    description: 'A RESTful API Guide to Machine Learning',
+    speaker: 'James Montgomery',
+    time: {
+      start: "2016-09-03T12:10:00.0Z",
+      end: "2016-09-03T13:00:00.0Z"
+    },
+    track: 'c',
+    id: 9
+// },  {
 //    title: '',
 //    description: '',
 //    speaker: '',
 //    time: {
-//      start: "2016-06-03T11:50:00.0Z",
-//      end: "2016-06-03T12:30:00.0Z"
-//    },
-//    track: 'b',
-//    id: 5
-//  }, {
-//    title: '',
-//    description: '',
-//    speaker: '',
-//    time: {
-//      start: "2016-06-03T11:50:00.0Z",
-//      end: "2016-06-03T12:30:00.0Z"
-//    },
-//    track: 'c',
-//    id: 6
-//  }, {
-//    title: '',
-//    description: '',
-//    speaker: '',
-//    time: {
-//      start: "2016-06-03T13:50:00.0Z",
-//      end: "2016-06-03T14:30:00.0Z"
+//      start: "2016-09-03T14:00:00.0Z",
+//      end: "2016-96-03T14:50:00.0Z"
 //    },
 //    track: 'a',
-//    id: 7
+//    id: 10
 //  }, {
 //    title: '',
 //    description: '',
 //    speaker: '',
 //    time: {
-//      start: "2016-06-03T13:50:00.0Z",
-//      end: "2016-06-03T14:30:00.0Z"
+//      start: "2016-09-03T14:00:00.0Z",
+//      end: "2016-09-03T14:50:00.0Z"
 //    },
 //    track: 'b',
-//    id: 8
+//    id: 11
 //  }, {
 //    title: '',
 //    description: '',
 //    speaker: '',
 //    time: {
-//      start: "2016-06-03T15:40:00.0Z",
-//      end: "2016-06-03T16:20:00.0Z"
+//      start: "2016-09-03T14:00:00.0Z",
+//      end: "2016-09-03T14:50:00.0Z"
 //    },
 //    track: 'c',
-//    id: 9
-  }];
+//    id: 12
+}];
 
 export default function scheduleReducer(state = initialState, action) {
   switch (action.type) {
